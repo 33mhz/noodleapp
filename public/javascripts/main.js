bc919ee3dbee2e227d42c33580875d666be9e13f
Paul Kalupnieks <paul@bigbangtechnology.com>
2012-09-28 13:13:37 -0400

    Updates jQuery to 1.8.2

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -4,7 +4,7 @@ requirejs.config({
   baseUrl: '/javascripts/lib',
   enforceDefine: true,
   paths: {
-    jquery: 'https://ajax.googleapis.com/ajax/libs/jquery/1.8.0/jquery.min'
+    jquery: 'https://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min'
   }
 });
 
