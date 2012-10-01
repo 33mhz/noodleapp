3767c2c92ae169f077a7f16ec1c5463d9ae6b7d6
Jen Fong-Adwent <jen@ednapiranha.com>
2012-10-01 17:46:26 -0400

    fixed media embed output

--- test/test.web-remix.js
+++ test/test.web-remix.js
@@ -10,24 +10,24 @@ describe('web-remix', function() {
   describe('.generate',  function() {
     it('returns embed code for a youtu.be short url', function(done) {
       webRemix.generate('http://youtu.be/5cazkHAHiPU', function(err, subject) {
-        subject.should.equal('<br><iframe width="530" height="298" src="//www.youtube.com/embed/5cazkHAHiPU?wmode=transparent" ' +
-        'frameborder="0" allowfullscreen></iframe><br>');
+        subject.should.equal('<div class="object-wrapper"><iframe width="530" height="298" src="//www.youtube.com/embed/5cazkHAHiPU?wmode=transparent" ' +
+        'frameborder="0" allowfullscreen></iframe></div>');
         done();
       });
     });
 
     it('returns embed code for a youtube normal url', function(done) {
       webRemix.generate('http://www.youtube.com/watch?v=5cazkHAHiPU', function(err, subject) {
-        subject.should.equal('<br><iframe width="530" height="298" src="//www.youtube.com/embed/5cazkHAHiPU?wmode=transparent" ' +
-          'frameborder="0" allowfullscreen></iframe><br>');
+        subject.should.equal('<div class="object-wrapper"><iframe width="530" height="298" src="//www.youtube.com/embed/5cazkHAHiPU?wmode=transparent" ' +
+          'frameborder="0" allowfullscreen></iframe></div>');
         done();
       });
     });
 
     it('returns embed code for a vimeo video url', function(done) {
       webRemix.generate('http://vimeo.com/37872583', function(err, subject) {
-        subject.should.equal('<br><iframe src="//player.vimeo.com/video/37872583" width="530" height="298" ' +
-          'frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe><br>');
+        subject.should.equal('<div class="object-wrapper"><iframe src="//player.vimeo.com/video/37872583" width="530" height="298" ' +
+          'frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe></div>');
         done();
       });
     });
@@ -53,7 +53,7 @@ describe('web-remix', function() {
           'target="_blank" style="color:#02a0c7; font-weight:bold;"> Mixcloud</a></p><div style="clear:both; ' +
           'height:3px;"></div></div>' });
       webRemix.generate('http://mixcloud.com/LuckyMe/25-jamie-vexd-sunday-walkman-mix/', function(err, subject) {
-        subject.should.equal('<br><div class="object-wrapper"><div><object width="300" height="300"><param name="movie" ' +
+        subject.should.equal('<div class="object-wrapper"><div class="object-wrapper"><div><object width="300" height="300"><param name="movie" ' +
           'value="//www.mixcloud.com/media/swf/player/mixcloudLoader.swf?feed=http%3A%2F%2Fwww.mixcloud.com%2F' +
           'lazykiki%2Fode-to-concrete%2F&amp;embed_uuid=2a3c7546-7bd1-482b-809c-0a0fa0f39095&amp;stylecolor=&amp;' +
           'embed_type=widget_standard"></param><param name="allowFullScreen" value="true"></param><param ' +
@@ -71,11 +71,11 @@ describe('web-remix', function() {
           'font-weight:bold;">Lazy Kiki</a><span> on </span><a href="http://www.mixcloud.com/?utm_source=' +
           'widget&amp;utm_medium=web&amp;utm_campaign=base_links&amp;utm_term=homepage_link" target="_blank" ' +
           'style="color:#02a0c7; font-weight:bold;"> Mixcloud</a></p><div style="clear:both; height:3px;">' +
-          '</div></div></div><br>');
+          '</div></div></div></div>');
       });
     });
 
-    it('returns oembed code for a soundcloud url', function(done) {
+    it('returns oembed code for a soundcloud url', function() {
       var scope = nock('soundcloud.com').get('/oembed?format=json&url=http//soundcloud.com/track').reply(200,
           { html: '<iframe src="//w.soundcloud.com/player/?url=http%3A' +
           '%2F%2Fapi.soundcloud.com%2Fplaylists%2F723408&amp;show_artwork=true" frameborder="no" height="450" ' +
@@ -83,10 +83,9 @@ describe('web-remix', function() {
           'href="http://soundcloud.com/skeptical/sets/tracks-576/">http://soundcloud.com/skeptical/sets' +
           '/tracks-576/</a>' });
       webRemix.generate('http://soundcloud.com/skeptical/sets/tracks-576/', function(err, subject) {
-        subject.should.equal('<br><iframe width="100%" height="450" scrolling="no" frameborder="no" ' +
+        subject.should.equal('<div class="object-wrapper"><iframe width="100%" height="450" scrolling="no" frameborder="no" ' +
           'src="//w.soundcloud.com/player/?url=http%3A%2F%2Fapi.soundcloud.com%2Fplaylists%2F723408&show_artwork=true">' +
-          '</iframe><br>');
-        done();
+          '</iframe></div>');
       });
     });
 
@@ -103,24 +102,24 @@ describe('web-remix', function() {
           'href="http://soundcloud.com/skeptical/sets/tracks-576/">http://soundcloud.com/skeptical/sets' +
           '/tracks-576/</a> test' });
       webRemix.generate('http://soundcloud.com/skeptical/sets/tracks-576/ test #tag', function(err, subject) {
-        subject.should.equal('test <a href="/tagged/tag">#tag</a> <br><iframe width="100%" height="450" scrolling="no" frameborder="no" ' +
+        subject.should.equal('test <a href="/tagged/tag">#tag</a> <div class="object-wrapper"><iframe width="100%" height="450" scrolling="no" frameborder="no" ' +
           'src="//w.soundcloud.com/player/?url=http%3A%2F%2Fapi.soundcloud.com%2Fplaylists%2F723408&show_artwork=true">' +
-          '</iframe><br>');
+          '</iframe></div>');
       });
     });
 
     it('returns embed code for a rd.io short url', function(done) {
       webRemix.generate('http://rd.io/i/QVME9DdeW1GL', function(err, subject) {
-        subject.should.equal('<br><iframe class="rdio" width="450" height="80" ' +
-          'src="//rd.io/i/QVME9DdeW1GL" frameborder="0"></iframe><br>');
+        subject.should.equal('<div class="object-wrapper"><iframe class="rdio" width="450" height="80" ' +
+          'src="//rd.io/i/QVME9DdeW1GL" frameborder="0"></iframe></div>');
         done();
       });
     });
 
     it('returns embed code for a rdio normal url', function(done) {
       webRemix.generate('http://rdio.com/x/QVME9DdeW1GL', function(err, subject) {
-        subject.should.equal('<br><iframe class="rdio" width="450" height="80" ' +
-          'src="//rd.io/i/QVME9DdeW1GL" frameborder="0"></iframe><br>');
+        subject.should.equal('<div class="object-wrapper"><iframe class="rdio" width="450" height="80" ' +
+          'src="//rd.io/i/QVME9DdeW1GL" frameborder="0"></iframe></div>');
         done();
       });
     });
@@ -128,15 +127,15 @@ describe('web-remix', function() {
     it('returns image code for an img url', function() {
       webRemix.generate('http://3.bp.blogspot.com/_K_1LxF4TvhU/S7UUE6PYKiI/AAAAAAAADto/XfpdX2CIxqY/' +
         's400/Riley+the+smiling+dog.jpg', function(err, subject) {
-        subject.should.equal('<br><div class="image-wrapper"><img src="http://3.bp.blogspot.com/_K_1LxF4TvhU/S7UUE6PYKiI/AAAAAAAADto/XfpdX2CIxqY/' +
-          's400/Riley+the+smiling+dog.jpg"></div><br>');
+        subject.should.equal('<div class="image-wrapper"><img src="http://3.bp.blogspot.com/_K_1LxF4TvhU/S7UUE6PYKiI/AAAAAAAADto/XfpdX2CIxqY/' +
+          's400/Riley+the+smiling+dog.jpg"></div>');
       });
     });
 
     it('returns image code for an instagr.am url', function() {
       webRemix.generate('http://instagram.com/p/QFJJzTw8yS/', function(err, subject) {
-        subject.should.equal('<br><div class="image-wrapper"><a href="http://instagram.com/p/QFJJzTw8yS/">' +
-          '<img src="http://instagr.am/p/QFJJzTw8yS/media/"/></a></div><br>');
+        subject.should.equal('<div class="image-wrapper"><a href="http://instagram.com/p/QFJJzTw8yS/">' +
+          '<img src="http://instagr.am/p/QFJJzTw8yS/media/"/></a></div>');
       });
     });
 
@@ -157,8 +156,8 @@ describe('web-remix', function() {
     it('returns video for a video link', function(done) {
       var video = 'http://blah.com/video.ogv';
       webRemix.generate(video, function(err, subject) {
-        subject.should.equal('<br><video controls="controls" preload="none" autobuffer><source src="' + video +
-          '" type="video/ogg; codecs="vp8, vorbis" /></video><br>');
+        subject.should.equal('<div class="object-wrapper"><video controls="controls" preload="none" autobuffer><source src="' + video +
+          '" type="video/ogg; codecs="vp8, vorbis" /></video></div>');
         done();
       });
     });
@@ -166,8 +165,8 @@ describe('web-remix', function() {
     it('returns audio for an audio link', function(done) {
       var audio = 'http://blah.com/audio.ogg';
       webRemix.generate(audio, function(err, subject) {
-        subject.should.equal('<br><audio controls="controls" preload="none" autobuffer><source src="' + audio +
-          '" type="audio/ogg" /></audio><br>');
+        subject.should.equal('<div class="object-wrapper"><audio controls="controls" preload="none" autobuffer><source src="' + audio +
+          '" type="audio/ogg" /></audio></div>');
         done();
       });
     });
