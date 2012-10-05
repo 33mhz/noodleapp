cad67674521fdaa1d1c2ff06adf58d1ff7220053
Jen Fong-Adwent <jen@ednapiranha.com>
2012-10-05 16:56:54 -0400

    updated version

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ var io = require('socket.io').listen(server);
 
 io.configure(function() {
   io.set('transports', ['xhr-polling']);
-  io.set('polling duration', 60);
+  io.set('polling duration', 30);
   io.set('log level', 1);
 });
 
