92b5388d0e2f60015599272799b6d3baa7d6a533
David Walsh <dwalsh@mozilla.com>
2012-10-30 10:11:26 -0500

    Focuses upon textarea within overlay when opened

--- public/javascripts/jquery.caret.js
+++ public/javascripts/jquery.caret.js
@@ -16,4 +16,16 @@ define(['jquery'],
       }
       return pos;
     }
+
+    $.fn.moveCursorToEnd = function() {
+      var el = $(this).get(0);
+      if (typeof el.selectionStart == 'number') {
+        el.selectionStart = el.selectionEnd = el.value.length;
+      } else if (typeof el.createTextRange != 'undefined') {
+        el.focus();
+        var range = el.createTextRange();
+        range.collapse(false);
+        range.select();
+      }
+    }
 });
