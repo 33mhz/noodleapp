e13b08ddfd5ce7c67c44b7d203566c8f037e8727
Matthew Flaschen <matthew.flaschen@gatech.edu>
2012-11-21 18:56:53 -0500

    Add stringify to turn entities back to markdown, with test.

--- test/test.markdown-to-entities.js
+++ test/test.markdown-to-entities.js
@@ -46,4 +46,39 @@ describe('markdown-to-entities', function() {
       done();
     });
   });
+
+  describe('stringify', function() {
+    it('returns correct markdown from text and links', function(done) {
+      var USER_DESCRIPTION = {
+        text: 'I\'m a chef at the Sabrosa restaurant.  Feel free to try some of our recipes.  In my spare time, I bike trails from http://trails.com/ and swim.',
+        entities: {
+          links: [
+            {
+              pos: 64,
+              len: 11,
+              text: 'our recipes',
+              url: 'http://tastyrecipes.com/sabrosa'
+            },
+            {
+              pos: 115,
+              len: 18,
+              text: 'http://trails.com/',
+              url: 'http://trails.com/'
+            },
+            {
+              pos: 18,
+              len: 18,
+              text: 'Sabrosa restaurant',
+              url: 'http://www.sabrosa.com'
+            }
+          ]
+        }
+      };
+
+      var EXPECTED_MARKDOWN = 'I\'m a chef at the [Sabrosa restaurant](http://www.sabrosa.com).  Feel free to try some of [our recipes](http://tastyrecipes.com/sabrosa).  In my spare time, I bike trails from http://trails.com/ and swim.';
+      var result = markdownToEntities.stringify(USER_DESCRIPTION);
+      result.should.eql(EXPECTED_MARKDOWN);
+      done();
+    });
+  });
 });
